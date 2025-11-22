abffedb2257e7293bfe7a4902cef939cc85bcb5a
passosthe <sthefanepassos@hotmail.com>
2025-11-22 20:44:01 -0300

    feat: criando-servidor-local

--- server.js
+++ server.js
@@ -1,2 +1,22 @@
+import http from "http";
 
+const PORT = 3000; // porta de comunicação utilizada nessa api
 
+const rotas = {
+    "/" : "Curso de Node.js",
+    "/livros" : "Entrei na rota livros",
+    "/autores" : "Entrei na rota autores"
+}
+
+//criando servidor local
+const server = http.createServer( ( req, res ) => {
+    res.writeHead(
+        200, //resposta
+        { "content-type": "text/plain" } //tipo de conteúdo
+    );
+    res.end(rotas[req.url]) //conteúdo
+})
+
+server.listen(PORT, () => {
+    console.log("servidor escutando!")
+});
